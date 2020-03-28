0dd20c30d2536ad013d928dfd045ce9032bff52a
Chris Boakes <chris@chrisboakes.com>
2020-03-28 15:49:26 +0000

    Basic content output

---
+++ src/components/Content.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let content;
+</script>
+
+<ul>
+    {#each content as item}
+		<li>
+            <article>
+                <header>
+                    <h2>{item.name}</h2>
+                </header>
+            </article>
+        </li>
+	{/each}
+</ul>
---
+++ src/components/Header.svelte
@@ -0,0 +1,2 @@
+<h1>Brighton Independent Shops</h1>
+<h2>Delivering During The Coronavirus Outbreak</h2>
